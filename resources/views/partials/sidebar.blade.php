d4453f84aba5950c392ef02f0dbef7f323319378
Erik Zerpa <zerpaerik@gmail.com>
2019-01-31 09:26:06 -0400

    cccc

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -241,6 +241,44 @@
 
             </li>
 
+                <li class="treeview">
+                <a href="#">
+                    <i class="fa fa-users"></i>
+                    <span class="title">@lang('global.user-management.title')</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+
+                    <li class="{{ $request->segment(2) == 'abilities' ? 'active active-sub' : '' }}">
+                        <a href="{{ route('admin.abilities.index') }}">
+                            <i class="fa fa-briefcase"></i>
+                            <span class="title">
+                                @lang('global.abilities.title')
+                            </span>
+                        </a>
+                    </li>
+                    <li class="{{ $request->segment(2) == 'roles' ? 'active active-sub' : '' }}">
+                        <a href="{{ route('admin.roles.index') }}">
+                            <i class="fa fa-briefcase"></i>
+                            <span class="title">
+                                @lang('global.roles.title')
+                            </span>
+                        </a>
+                    </li>
+                    <li class="{{ $request->segment(2) == 'users' ? 'active active-sub' : '' }}">
+                        <a href="{{ route('admin.users.index') }}">
+                            <i class="fa fa-user"></i>
+                            <span class="title">
+                                @lang('global.users.title')
+                            </span>
+                        </a>
+                    </li>
+               
+                </ul>
+            </li>
+
    
             
             @endcan
